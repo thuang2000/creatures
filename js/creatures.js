2a176385ceba9b95ce267c0b01eabd7e43f7c792
thuang2000 <thuangnsc@gmail.com>
2026-05-19 05:14:06 +0000

    Adds switches

--- js/creatures.js
+++ js/creatures.js
@@ -11,7 +11,25 @@ function showHome()
         
         */
         document.querySelectorAll("div.creature").forEach(el => {
-            el.style.backgroundColor = "yellow";
+            //el.style.backgroundColor = "yellow";
+
+            let myHome = el.dataset.home;
+            //alert(myHome);
+            switch(myHome){
+                case "land":
+                    el.style.backgroundColor = "green";
+                break;
+
+                case "water":
+                    el.style.backgroundColor = "blue";
+                break;
+
+                case "air":
+                    el.style.backgroundColor = "yellow";
+                break;
+
+
+            }
 });
 
     
@@ -25,7 +43,25 @@ function showType()
         if creature is an animal, border color solid red
         if creature is an insect, border color is dashed orange
     */
-    alert("showType()");
+            document.querySelectorAll("div.creature").forEach(el => {
+            //el.style.backgroundColor = "yellow";
+
+            let myType = el.dataset.creatureType;
+            
+            switch(myType){
+                case "animal":
+                    el.style.border = "3px solid red";
+                break;
+            
+                case "insect":
+                    el.style.border = "3px dashed orange";
+                break;
+
+                
+
+
+            }
+});
 }//end showType()
 
 function clearAll()
@@ -37,5 +73,8 @@ function clearAll()
         clear background color to white
         
     */
-    alert("clearAll()");
+    document.querySelectorAll("div.creature").forEach(function(el){
+        el.style.backgroundColor = "#FFF";
+        el.style.border = "2px solid #000";
+    });
 }//end clearAll()
